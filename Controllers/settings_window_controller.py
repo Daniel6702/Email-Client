faadee845cb3a4e49b8094a7e0cefcac2832389d
Andreas-Bugge <202204219@post.au.dk>
2023-11-24 12:21:03 +0100

    Switch account feature

--- Controllers/settings_window_controller.py
+++ Controllers/settings_window_controller.py
@@ -4,9 +4,11 @@ from Views.styles.style_manager import StyleManager
 from EmailService.models.email_client import EmailClient
 from EmailService.models import User
 from PyQt5.QtCore import pyqtSignal
+from EmailService.services.common.user_manager_service import restart_program
 
 class SettingsWindowController(QWidget):
     close_window_signal = pyqtSignal()
+    open_login_signal = pyqtSignal()
     def __init__(self, style_manager: StyleManager, client):
         super().__init__()
         self.style_manager = style_manager
@@ -17,12 +19,16 @@ class SettingsWindowController(QWidget):
     def setup_connections(self):
         self.settings_window.style_signal.connect(self.on_style_changed)
         self.settings_window.logout_signal.connect(self.delete)
+        self.settings_window.switch_account_signal.connect(self.switch_account)
 
     def on_style_changed(self, style: str):
         self.style_manager.set_style(style)
 
     def show_settings(self):
         self.settings_window.show()
+
+    def switch_account(self):
+        restart_program()
     
     def delete(self):
         current_user = self.email_client.get_user()
@@ -30,3 +36,4 @@ class SettingsWindowController(QWidget):
         self.close_window_signal.emit()
         self.settings_window.close()
         
+    
